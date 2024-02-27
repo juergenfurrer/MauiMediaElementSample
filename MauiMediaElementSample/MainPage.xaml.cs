769fb45ad38138bbed42bd09eb8bb48095d0044a
Juergen Furrer <juergen.furrer@gmail.com>
2024-02-27 22:08:38 +0100

    Initial deploy

---
+++ MauiMediaElementSample/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiMediaElementSample;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+    }
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		if (mediaElement.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
+			mediaElement.Pause();
+		else if (mediaElement.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused)
+			mediaElement.Play();
+    }
+}
+
